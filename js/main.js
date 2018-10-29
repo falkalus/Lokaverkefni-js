4fd9b1475efd7eef7a1da9b9567835f164362840
falkalus <43388421+falkalus@users.noreply.github.com>
2018-10-29 12:15:40 +0000

    Folders
    Folders

---
+++ js/main.js
@@ -0,0 +1,34 @@
+/* sýnið að þið kunnið ekki einblína á listan
+Data Types= -Boolean -Number -String -Object -Array -Function 
+
+Operators= +, -, * eða /
+<, <=, ===, !==, >= eða >
+|| eða &&
++=, -=, ++ eða --
+
+Conditionals= -If - else if - else
+
+Functions= -Parameters -Arguments -Return -Events -Calling a function 
+
+Loops= -For eða while
+*/
+
+/*gagnvirk saga
+birta eina blaðsíðu í einu 
+leyfir notandanum svo að velja hvað sögupersónan gerir áður en sagan heldur áfram 
+Hvort tekur þú bláu eða rauðu?” 
+þá væri mynd af rauðri pillu og blárri sem notandinn gæti smellt á 
+og svo heldur sagan áfram eftir því hvað notandinn gerði. 
+Það væri líka hægt að bjóða uppá að skrifa eitthvað svar inn í <input type=”text” />
+*/
+
+/* you name
+There once was a beautifull princess who many wanted to marry but she chose __
+Unfortunatly soon after the weeding the princess fell ill, griefstricken
+ __ wished to be sealed inside the crypt with her
+ After a few days __ heard a hissing noice coming from one of the crecks
+ It was a snake that crawled into the crypt with them
+ __ feared the snake was evil, What did __ do
+ option a
+ option b*/
+
